b4afba55218233f45a196a2963e42766aac83e54
Your Name <you@example.com>
2024-03-24 13:06:21 +0000

    counter added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import '../../../public/font.css'
 const Blogs = () => {
     return (
-        <div>
-            <h1>Blogs</h1>
+        <div className='font'>
+            <h1>Blogs Comming Soon</h1>
+            <div className="flex gap-5 justify-center">
+                <div>
+                    <span className="countdown font-mono text-4xl">
+                        <span style={{ "--value": 15 }}></span>
+                    </span>
+                    days
+                </div>
+                <div>
+                    <span className="countdown font-mono text-4xl">
+                        <span style={{ "--value": 10 }}></span>
+                    </span>
+                    hours
+                </div>
+                <div>
+                    <span className="countdown font-mono text-4xl">
+                        <span style={{ "--value": 24 }}></span>
+                    </span>
+                    min
+                </div>
+                <div>
+                    <span className="countdown font-mono text-4xl">
+                        <span style={{ "--value": 51 }}></span>
+                    </span>
+                    sec
+                </div>
+            </div>
         </div>
     );
 };
